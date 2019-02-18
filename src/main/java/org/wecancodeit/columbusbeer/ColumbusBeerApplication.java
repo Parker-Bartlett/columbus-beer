50d4132a38104f5d263d2ab9d49eea4a1b7cbdbe
Parker Bartlett <prbartlett1@gmail.com>
2019-02-18 14:02:54 -0500

    initial commit

---
+++ src/main/java/org/wecancodeit/columbusbeer/ColumbusBeerApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.columbusbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumbusBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColumbusBeerApplication.class, args);
+	}
+
+}
