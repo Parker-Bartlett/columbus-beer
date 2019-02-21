71907f441c85175c0c02b420f3248192087d3d0e
Parker Bartlett <prbartlett1@gmail.com>
2019-02-21 11:25:04 -0500

    testing

--- src/main/java/org/wecancodeit/columbusbeer/controllers/HomeController.java
+++ src/main/java/org/wecancodeit/columbusbeer/controllers/HomeController.java
@@ -1,13 +1,39 @@
 package org.wecancodeit.columbusbeer.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.wecancodeit.columbusbeer.Form;
+import org.wecancodeit.columbusbeer.repositories.FormsRepository;
 
 @Controller
 public class HomeController {
-	
+
+	private FormsRepository forms = new FormsRepository(new ArrayList<Form>());
+
 	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
-} 
+
+	@GetMapping("/form")
+	public String greetingForm(Model model) {
+		model.addAttribute("forms", forms.getForms());
+		return "form";
+	}
+
+	@GetMapping("/formresult")
+	public String formResult() {
+		return "formresult";
+	}
+	@PostMapping("/form")
+	public String greetingSubmit(String name, String review) {
+		forms.addForm(new Form(name, review));
+		return "redirect:/formresult";
+	}
+	
+}
